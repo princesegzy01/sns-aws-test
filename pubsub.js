886293db3f999838995d73290ec168ea4754a136
Segun Sodimu <princesegzy01@yahoo.com>
2022-01-07 09:38:18 +0100

    update codebase

--- pubsub.js
+++ pubsub.js
@@ -1,8 +1,12 @@
 
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
 // const port = 4500
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4500;
 
 
 // Load the AWS SDK for Node.js
@@ -11,25 +15,25 @@ var AWS = require('aws-sdk');
 AWS.config.update({region: 'eu-west-2'});
 
 
-const params = {
-  TopicArn : process.env.ARN,
-  Protocol: 'https',
-  Endpoint : 'https://sns-pub-sub.herokuapp.com/events'
+// const params = {
+//   TopicArn : process.env.ARN,
+//   Protocol: 'https',
+//   Endpoint : 'https://sns-pub-sub.herokuapp.com/events'
 
-}
+// }
 
-const sns = new AWS.SNS({apiVersion: '2010-03-31'});
+// const sns = new AWS.SNS({apiVersion: '2010-03-31'});
 
-sns.subscribe(params, (err, data) => {
-    if (err) {
-        console.log("<<<<<<<<<<<<<<<<<<<<< Error")
-        console.log(err);
-    } else {
-        console.log(">>>>>>>>>>>>>>>>>> Success")
-        console.log(data);
-        // console.send(data);
-    }
-});
+// sns.subscribe(params, (err, data) => {
+//     if (err) {
+//         console.log("<<<<<<<<<<<<<<<<<<<<< Error")
+//         console.log(err);
+//     } else {
+//         console.log(">>>>>>>>>>>>>>>>>> Success")
+//         console.log(data);
+//         // console.send(data);
+//     }
+// });
 
 
 
@@ -39,8 +43,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/events', (req, res) => {
-    console.log(req.body)
+app.post('/events', (request, res) => {
+    console.log(request.body)
     res.send("This is good")
   }) 
 
